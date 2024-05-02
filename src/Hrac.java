25908254251d877438caed983b62048bac4d44d3
InkaEnFu <Inkaenfu69@gmail.com>
2024-05-02 17:47:39 +0200

    Created method for painting player image

--- src/Hrac.java
+++ src/Hrac.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 
@@ -9,4 +10,15 @@ public class Hrac  {
     private int deltaX = 0;
     private int deltaY = 0;
     private BufferedImage postavaImage;
+
+    public void vykreslit(Graphics g) {
+        if (postavaImage != null) {
+            g.drawImage(postavaImage, x, y, herniPole.VELIKOST_KOSTICKY, herniPole.VELIKOST_KOSTICKY, null);
+        }
+    }
+
+
+
+
+
 }
