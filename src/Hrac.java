6cf3924ec0d3073eb95fa932e39e755e7fe0f123
InkaEnFu <Inkaenfu69@gmail.com>
2024-05-02 18:10:49 +0200

    Created method for moving across gameboard.

--- src/Hrac.java
+++ src/Hrac.java
@@ -1,7 +1,10 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Hrac implements KeyListener {
     private int x;
@@ -12,11 +15,36 @@ public class Hrac implements KeyListener {
     private int deltaY = 0;
     private BufferedImage postavaImage;
 
+
+
     public void vykreslit(Graphics g) {
         if (postavaImage != null) {
             g.drawImage(postavaImage, x, y, herniPole.VELIKOST_KOSTICKY, herniPole.VELIKOST_KOSTICKY, null);
         }
     }
+    private void pohyb() {
+        while (true) {
+            int novaX = x + (deltaX != 0 ? (deltaX / Math.abs(deltaX)) * herniPole.VELIKOST_KOSTICKY : 0);
+            int novaY = y + (deltaY != 0 ? (deltaY / Math.abs(deltaY)) * herniPole.VELIKOST_KOSTICKY : 0);
+
+            if (novaX >= 0 && novaX <= (herniPole.POCET_SLOUPCU - 1) * herniPole.VELIKOST_KOSTICKY &&
+                    novaY >= 0 && novaY <= (herniPole.POCET_RADKU - 1) * herniPole.VELIKOST_KOSTICKY &&
+                    herniPole.getMapa().getPolicko(novaX / herniPole.VELIKOST_KOSTICKY, novaY / herniPole.VELIKOST_KOSTICKY) == 0) {
+                x = novaX;
+                y = novaY;
+                herniPole.repaint();
+            }
+
+            try {
+                Thread.sleep(130);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
     @Override
     public void keyTyped(KeyEvent e) {
     }
@@ -60,12 +88,5 @@ public class Hrac implements KeyListener {
                 break;
         }
     }
-
-
-
-
-
-
-
 
 }
