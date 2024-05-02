8cf86132376e65c19e1f906f22fd846bac8e43df
InkaEnFu <Inkaenfu69@gmail.com>
2024-05-02 17:54:36 +0200

    Created method for basic character movement.

--- src/Hrac.java
+++ src/Hrac.java
@@ -1,8 +1,9 @@
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 
-public class Hrac  {
+public class Hrac implements KeyListener {
     private int x;
     private int y;
     private int rychlost;
@@ -16,6 +17,52 @@ public class Hrac  {
             g.drawImage(postavaImage, x, y, herniPole.VELIKOST_KOSTICKY, herniPole.VELIKOST_KOSTICKY, null);
         }
     }
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        switch (key) {
+            case KeyEvent.VK_W:
+                deltaY = -herniPole.VELIKOST_KOSTICKY;
+                deltaX = 0;
+                break;
+            case KeyEvent.VK_S:
+                deltaY = herniPole.VELIKOST_KOSTICKY;
+                deltaX = 0;
+                break;
+            case KeyEvent.VK_A:
+                deltaX = -herniPole.VELIKOST_KOSTICKY;
+                deltaY = 0;
+                break;
+            case KeyEvent.VK_D:
+                deltaX = herniPole.VELIKOST_KOSTICKY;
+                deltaY = 0;
+                break;
+
+        }
+        herniPole.repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        switch (key) {
+            case KeyEvent.VK_W:
+            case KeyEvent.VK_S:
+                deltaY = 0;
+                break;
+            case KeyEvent.VK_A:
+            case KeyEvent.VK_D:
+                deltaX = 0;
+                break;
+        }
+    }
+
+
+
 
 
 
